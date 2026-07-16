524d21012cf6c138a05490ad6a7f1c8d4596fc89
Neal Itharaja <sitharaj@ucsc.edu>
2026-07-16 16:07:31 -0700

    Started Montgomery Reduction algorithm

--- src/arithmetic/montogomery.v
+++ src/arithmetic/montogomery.v
@@ -1,1 +1,10 @@
 // Montgomery reduction
+// T will be a product from mod_mult
+module montgomery(
+    input [11:0] T,
+    output [11:0] t
+);
+
+    paramter N = 3329;
+    parameter R = 4096;
+endmodule
