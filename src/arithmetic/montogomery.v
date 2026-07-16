e67116ab6129f5bed214bb95fd18459a6bfc5e10
Neal Itharaja <sitharaj@ucsc.edu>
2026-07-16 16:34:26 -0700

    Created Montgomery Reduction algorithm

--- src/arithmetic/montogomery.v
+++ src/arithmetic/montogomery.v
@@ -1,10 +1,26 @@
 // Montgomery reduction
 // T will be a product from mod_mult
 module montgomery(
-    input [11:0] T,
-    output [11:0] t
+    input [23:0] T,
+    output reg [11:0] t
 );
 
-    paramter N = 3329;
-    parameter R = 4096;
+    parameter [11:0] N = 3329;
+    parameter [11:0] N_Prime = 3327;
+
+    wire [11:0] m;
+    wire [24:0] inter;
+    wire [12:0] t_inter;
+
+    assign m = (T * N_Prime);
+    assign inter = T + (m*N);
+    assign t_inter = inter >> 12;
+
+    always @(*) begin
+        if (t_inter >= N) begin
+            t = t_inter - N;
+        end else begin
+            t = t_inter;
+        end
+    end
 endmodule
