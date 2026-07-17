e135d76b83528fb2d4c9af2fb602e383838c9be2
Neal Itharaja <sitharaj@ucsc.edu>
2026-07-17 16:52:25 -0700

    Debugging

--- src/arithmetic/to_montgomery.v
+++ src/arithmetic/to_montgomery.v
@@ -1,13 +1,17 @@
 // Turns input into Montogomery domain
 module to_mont(
     input [11:0] A,
-    output [11:0] A_Prime,
+    output [11:0] A_Prime
 );
 
     parameter [11:0] R_Sq_mod = 1353; // Uses R^2 mod q constant value
+    
+    wire [23:0] product;
+    
+    assign product = A * R_Sq_mod;
 
     montgomery APrime (
-        .T(A * R_Sq_mod),
+        .T(product),
         .t(A_Prime)
     );
 endmodule
