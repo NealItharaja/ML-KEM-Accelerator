78abfdd1255abf4ee652d448de01ee7489598992
Neal Itharaja <sitharaj@ucsc.edu>
2026-07-17 15:58:37 -0700

    Created modular multiplication module

--- src/arithmetic/mod_mult.v
+++ src/arithmetic/mod_mult.v
@@ -1,9 +1,20 @@
 // Modular Multiplication
-
-module
+// Using same R = 4096 (2^12)
+module mod_mult(
     input [11:0] A,
     input [11:0] B,
     output [11:0] result
+);
+
+    parameter [11:0] N = 3329;
+    parameter [11:0] N_Prime = 3327;
     
     wire [23:0] product;
+
+    assign product = A * B;
+
+    montgomery A_Prime (
+        .T(product),
+        .t(result)
+    )
 endmodule
