d5009fdc7f3073cb4962bcf8875c652a72ef6d15
Neal Itharaja <sitharaj@ucsc.edu>
2026-07-17 17:10:11 -0700

    Debugging

--- src/arithmetic/from_montgomery.v
+++ src/arithmetic/from_montgomery.v
@@ -2,8 +2,12 @@ module from_mont(
     input [11:0] C,
     output [11:0] result
 );
+    wire [23:0] extended_C;
+
+    assign extended_C = {12'd0, C};
+
     montgomery Result (
-        .T(C),
+        .T(extended_C),
         .t(result)
     );
 endmodule
