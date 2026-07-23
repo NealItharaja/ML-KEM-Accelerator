c61b206ab9bdf45c67d868a1840b44e42cd18832
Neal Itharaja <sitharaj@ucsc.edu>
2026-07-23 13:54:50 -0700

    Created RAM file for coefficients

---
+++ src/memory/coeff_ram.v
@@ -0,0 +1,17 @@
+// Storage for coefficients
+module coeff_ram(
+    input clk,
+    input we,
+    input [7:0] addr,
+    input [11:0] din,
+    output reg [11:0] dout
+);
+    
+    reg [11:0] mem [0:255];
+
+    always @(posedge clk) begin
+        if (we)
+            mem[addr] <= din'
+        dout <= mem[addr];
+    end 
+endmodule
