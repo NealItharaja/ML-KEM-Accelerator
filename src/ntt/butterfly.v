bbf7ac8f98febc6b34a9d47d70486f853df7f584
Neal Itharaja <sitharaj@ucsc.edu>
2026-07-29 12:51:39 -0700

    Created butterfly operations files

--- src/ntt/butterfly.v
+++ src/ntt/butterfly.v
@@ -1,1 +1,30 @@
 // Butterfly operations
+module butterfly(
+    input clk,
+    input [11:0] a,
+    input [11:0] b,
+    input [11:0] twiddle,
+    output [11:0] a_out,
+    output [11:0] b_out,
+    );
+
+    wire [11:0] T;
+
+    mod_mult product (
+        .A(b),
+        .B(twiddle),
+        .result(T)
+    );
+
+    mod_add add (
+        .A(a),
+        .B(T),
+        .result(a_out)
+    );
+
+    mod_sub sub (
+        .A(a),
+        .B(T),
+        .result(b_out)
+    );
+endmodule
