866efa90ca0b63aac82dead3f97ff490e00f8c8c
Neal Itharaja <sitharaj@ucsc.edu>
2026-08-04 01:41:04 -0700

    Testing pack.v

--- src/encode/decompress.v
+++ src/encode/decompress.v
@@ -1,12 +1,1 @@
 // Data decompression
-// 'Packing' data: converts coefficients into byte streams
-module decompress(
-    input clk,
-    input start,
-    input [9:0] coeff_in,
-    input coeff_valid,
-    output reg [11:0] coeff_out,
-    output reg coeff_valid,
-    output reg done
-    );
-endmodule
